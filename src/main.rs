44df0353c10d3fb1b0332532288891a7755a959a
gcb <descartavel1@gmail.com>
2017-11-26 22:58:18 -0800

    bg color. moving away from str.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,105 @@
+extern crate termion;
+use termion::{color};
+
+/** quick n dirty just to build and get some output **/
 fn main() {
-    println!("{}", fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq"));
+    print_board( fen_to_board(
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq"
+    ));
+    println!();
+    print_board( fen_to_board(
+        "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"
+    ));
 }
 
+/** prints a board adding terminal color **/
+// TODO: last move highlight
+// TODO: castling availability
+fn print_board ( board: [[char; 8]; 8] ) {
+    //println!("{:?}", board);
+    for row in 0..8 {
+        for col in 0..8 {
+            match (col+row)%2 {
+                0 => 
+                    print!("{}{}", color::Bg(color::Red), board[row][col] ),
+                _ => 
+                    print!("{}{}", color::Bg(color::Black), board[row][col] )
+            }
+        }
+        println!("{}", color::Bg(color::Reset) );
+    }
+
+}
 /** convert Forsyth-Edwards Notation to a UTF-8 string with a board */
-fn fen_to_board (input_str: &str) -> String {
-    let mut out = String::with_capacity(64);
+fn fen_to_board (input_str: &str) -> [[char; 8]; 8] {
+    // TODO must be a less asinine way to generate the next var
+    let mut out = [
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',],
+        [' ',' ',' ',' ',' ',' ',' ',' ',]];
+    let mut col = 0;
+    let mut row = 0;
     for (inter_index, inter_content) in input_str.chars().enumerate() {
-        if inter_index > 64 {
-            // notation we will use might have captured pieces after board
+        //println!("{},{} = {}", row, col, inter_content);
+        if col > 7 {
+            col = 0;
+            row +=1;
+        }
+        if row > 7 {
             break;
         }
-        out.push( piece_from_char(inter_content) );
+        if inter_content == '8'{
+            row += 1;
+            col = 0;
+            continue;
+        } else if inter_content == '7' {
+            col += 7;
+            continue;
+        } else if inter_content == '6' {
+            col += 6;
+            continue;
+        } else if inter_content == '5' {
+            col += 5;
+            continue;
+        } else if inter_content == '4' {
+            col += 4;
+            continue;
+        } else if inter_content == '3' {
+            col += 3;
+            continue;
+        } else if inter_content == '2' {
+            col += 2;
+            continue;
+        } else if inter_content == '1' {
+            col += 1;
+            continue;
+        } else if inter_content == '/' {
+            continue;
+        }
+        out[row][col] = piece_from_char(inter_content);
+        //
+        col+=1;
     }
     return out;
 }
 
 
-/** naive map to the utf-8 chars we want to draw **/
+/** naive map to the UTF-8 chars we want to draw **/
 fn piece_from_char( input_char :char ) -> char {
     match input_char {
-        '/' => '\n', // FEN use this. we can optimize and assume the breaks later on
+        // black
         'n' => '♘',
         'r' => '♖',
         'b' => '♗',
         'q' => '♕',
         'k' => '♔',
         'p' => '♙',
+        // white
         'N' => '♞',
         'R' => '♜',
         'B' => '♝',
